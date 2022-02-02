0acdbd9ba3a3f74cf5b1c50ff95af85c4e5434eb
Chris <mafradesouza.cs@gmail.com>
2022-02-02 03:01:48 -0300

    App.vue: change font-family and background color

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,17 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 
   height: 100vh;
 }
 body {
   margin: 0;
+  background-color: #f5f2fd;
 }
 </style>
