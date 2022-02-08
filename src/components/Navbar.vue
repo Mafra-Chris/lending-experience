683670e744d050010d2f063857585ddff1285340
Chris <mafradesouza.cs@gmail.com>
2022-02-08 10:21:40 -0300

    Navbar: add logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,19 @@
     text-color="#fff"
     active-text-color="#77baff"
   >
-    >
+    <el-menu-item
+      ><img src="@/assets/logo.png" alt="" class="logo"
+    /></el-menu-item>
+
     <el-menu-item index="1">Home</el-menu-item>
+    <el-menu-item index="2" class="btn-logout"
+      ><font-awesome-icon
+        icon="sign-out-alt"
+        size="lg"
+        class="logout-icon"
+      ></font-awesome-icon>
+      Log Out</el-menu-item
+    >
   </el-menu>
 </template>
 
@@ -18,4 +29,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.btn-logout {
+  right: 0;
+}
+
+.logout-icon {
+  margin-right: 0.25rem;
+}
+
+.navbar {
+  display: flex;
+}
+ul > :last-child {
+  margin-left: auto;
+}
+</style>
