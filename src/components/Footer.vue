08f01c1213e78c608beb034864792a392287341a
Chris <mafradesouza.cs@gmail.com>
2022-02-08 10:14:29 -0300

    create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+    <img src="@/assets/wave-haikei.svg" alt="" srcset="" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  margin-bottom: -1rem;
+  width: 100%;
+  z-index: -1;
+}
+</style>
