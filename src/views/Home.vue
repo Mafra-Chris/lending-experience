e8c3f7fe747bbe7b422e773068198349d342dc1a
Chris <mafradesouza.cs@gmail.com>
2022-02-08 10:20:27 -0300

    Home: make responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,40 @@
 <template>
   <div>
+    <ErrorDialog
+      :message="errorMessage.message"
+      :status="errorMessage.status"
+      :isDialogVisible="isError"
+    />
+
     <Navbar />
     <div class="main" v-if="isLoaded">
       <div class="main-title">
-        <h1>Parabéns, a55</h1>
-        <h2>Agora você pode chegar ao próximo nível!</h2>
+        <h1>Parabéns, {{ companyName }}</h1>
+        <h2 class="main-subtitle">
+          Agora você está mais perto de chegar ao próximo nível!
+        </h2>
       </div>
       <el-row>
-        <el-col :sm="12" class="hidden-xs-only chosen-offer">
+        <el-col :sm="12" class="chosen-offer">
           <ProposalCard
             :interestRate="chosenOffer.interest"
             :monthlyRevenue="monthlyRevenue"
             :amountPerc="chosenOffer.amountPerc"
+            :isChosen="true"
+            :installmentsChosen="chosenInstallments"
+            :taxValue="chosenOffer.taxValue"
           />
         </el-col>
-        <el-col :xs="24" :sm="12">
-          <img class="money-img" src="@/assets/banknote-animate.svg" alt=""
-        /></el-col>
+        <el-col :xs="24" :sm="12" class="hidden-xs-only banknote-col">
+          <img class="money-img" src="@/assets/banknote-animate.svg" alt="" />
+        </el-col>
       </el-row>
     </div>
+
+    <div v-if="isError">
+      <img style="height: 90vh" src="@/assets/No-data-rafiki.svg" alt="" />
+    </div>
+    <Footer />
   </div>
 </template>
 
@@ -26,46 +42,100 @@
 import Vue from 'vue';
 import Navbar from '@/components/Navbar.vue';
 import ProposalCard from '@/components/ProposalCard.vue';
+import Footer from '@/components/Footer.vue';
+import ErrorDialog from '@/components/ErrorDialog.vue';
 import axios, { AxiosResponse } from 'axios';
+import 'element-ui/lib/theme-chalk/display.css';
 import { Offer } from '@/types/offer';
+import { ErrorMessage } from '@/types/error';
+import { AtomSpinner } from 'epic-spinners';
 
 export default Vue.extend({
   name: 'Home',
-  components: { Navbar, ProposalCard },
+  components: { Navbar, ProposalCard, Footer, ErrorDialog, AtomSpinner },
   data() {
     return {
       monthlyRevenue: 0,
       chosenOffer: {} as Offer,
       isLoaded: false,
+      isError: false,
+      errorMessage: {
+        message: '',
+        status: 0,
+      } as ErrorMessage,
     };
   },
-  created() {
-    axios
+  mounted: async function () {
+    let flag = true;
+
+    await axios
       .get('https://61fb29d587801d0017a2c40d.mockapi.io/userNormal')
       .then(async (response: AxiosResponse) => {
-        console.log(response);
         this.monthlyRevenue = response.data.monthlyRevenue;
+      })
+      .catch((error) => {
+        flag = false;
+        this.errorMessage = {
+          message: 'Erro ao buscar valor de receita mensal.',
+          status: error.response.status,
+        };
       });
-    axios
-      .get('https://61fb29d587801d0017a2c40d.mockapi.io/offers/2')
-      .then(async (response: AxiosResponse) => {
-        this.isLoaded = true;
-        this.chosenOffer = response.data;
-      });
+
+    if (flag) {
+      await axios
+        .get('https://61fb29d587801d0017a2c40d.mockapi.io/offers/2')
+        .then(async (response: AxiosResponse) => {
+          Object.assign(this.chosenOffer, response.data);
+          this.isLoaded = true;
+          console.log(this.$store.state.user.offer.id);
+        })
+        .catch((error) => {
+          flag = false;
+          this.errorMessage = {
+            message: 'Erro ao buscar proposta escolhida.',
+            status: error.response.status,
+          };
+        });
+    }
+
+    if (!flag) {
+      this.isError = true;
+    }
+  },
+  computed: {
+    chosenInstallments(): number {
+      return this.$store.state.user.offer.installments;
+    },
+    companyName(): string {
+      return this.$store.state.signup.name;
+    },
   },
 });
 </script>
 
 <style scoped>
 .money-img {
-  width: 80%;
+  width: 100%;
   float: left;
+  filter: hue-rotate(4deg) brightness(1.2);
+}
+
+@media (min-width: 1024px) {
+  .money-img {
+    width: 80%;
+    float: left;
+    filter: hue-rotate(4deg) brightness(1.2);
+  }
 }
 .main {
   margin-top: 3rem;
 }
 .main-title {
   margin-bottom: 2rem;
+}
+.main-subtitle {
+  color: #bbbabf;
+  font-weight: 300;
 }
 .chosen-offer {
   display: flex;
